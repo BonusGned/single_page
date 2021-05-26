37d9366deb96f510634b484771e6534689d9ef78
BonusGned <62764420+BonusGned@users.noreply.github.com>
2021-05-26 22:40:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import json
+
+from flask import Flask, render_template, jsonify, request, make_response
 from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
 
@@ -10,9 +12,37 @@ db = SQLAlchemy(app)
 from models import Table
 
 
-@app.route('/')
-def index():
-    fields = Table.query.all()
+@app.route('/', methods=['GET', 'POST'], defaults={'page': 1})
+@app.route('/<int:page>', methods=['GET', 'POST'])
+def index(page):
+    fields = Table.query.paginate(page=page, per_page=5, error_out=False)
+
+    if request.method == 'POST':
+        condition = request.form['condition']
+        column = request.form['column']
+        search_value = request.form['search_value']
+
+        if condition == 'contains' and column == 'name':
+            fields = Table.query.filter(Table.name.like(search_value)).paginate(page=page, per_page=5, error_out=False)
+
+        elif column == 'quantity':
+
+            if condition == '=':
+                fields = Table.query.filter(Table.quantity == int(search_value)).paginate(page=page, per_page=5, error_out=False)
+            elif condition == '>':
+                fields = Table.query.filter(Table.quantity < int(search_value)).paginate(page=page, per_page=5, error_out=False)
+            elif condition == '<':
+                fields = Table.query.filter(Table.quantity > int(search_value)).paginate(page=page, per_page=5, error_out=False)
+
+        else:
+
+            if condition == '=':
+                fields = Table.query.filter(Table.distance == int(search_value)).paginate(page=page, per_page=5, error_out=False)
+            elif condition == '>':
+                fields = Table.query.filter(Table.distance < int(search_value)).paginate(page=page, per_page=5, error_out=False)
+            elif condition == '<':
+                fields = Table.query.filter(Table.distance > int(search_value)).paginate(page=page, per_page=5, error_out=False)
+
     return render_template('tables/index.html', fields=fields)
 
 
